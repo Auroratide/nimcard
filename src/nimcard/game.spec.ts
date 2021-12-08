e78d8ce58e36375987331cae881af0109121084e
Timothy Foster <tf.auroratide@gmail.com>
2021-12-08 14:01:56 -0600

    Can play a game

---
+++ src/nimcard/game.spec.ts
@@ -0,0 +1,64 @@
+import { expect } from '@open-wc/testing'
+import { Board } from './board'
+import { Card } from './card'
+import { Game } from './game'
+
+describe('game', () => {
+    // Produces the game:
+    // J 2 A
+    // Q 2 8
+    const deck = [
+        { value: Card.Value.Queen, suit: Card.Suit.Diamonds, },
+        { value: Card.Value.Jack, suit: Card.Suit.Hearts, },
+        { value: Card.Value.Two, suit: Card.Suit.Spades, },
+        { value: Card.Value.Ace, suit: Card.Suit.Spades, },
+        { value: Card.Value.Two, suit: Card.Suit.Clubs, },
+        { value: Card.Value.Eight, suit: Card.Suit.Diamonds, },
+    ]
+
+    const rowAndAmount = (option: Game.Option) => ({ row: option.row, amount: option.amount })
+    const option = (row: number, amount: number) => (o: Game.Option) => o.row === row && o.amount === amount
+    
+    it('playing a game', () => {
+        const board = Board.create(deck, [3, 3])
+        let game = Game.start(board)
+
+        let options = Game.options(game)
+        expect(options).to.have.length(4)
+        expect(options.map(rowAndAmount)).to.deep.include({ row: 0, amount: 1 })
+        expect(options.map(rowAndAmount)).to.deep.include({ row: 0, amount: 2 })
+        expect(options.map(rowAndAmount)).to.deep.include({ row: 1, amount: 1 })
+        expect(options.map(rowAndAmount)).to.deep.include({ row: 1, amount: 2 })
+
+        // P1 chooses 2 and A from first row
+        game = options.find(option(0, 2))!.commit()
+        expect(Game.scores(game)).to.deep.equal([13, 0])
+
+        options = Game.options(game)
+        expect(options).to.have.length(3)
+        expect(options.map(rowAndAmount)).to.deep.include({ row: 0, amount: 1 })
+        expect(options.map(rowAndAmount)).to.deep.include({ row: 1, amount: 1 })
+        expect(options.map(rowAndAmount)).to.deep.include({ row: 1, amount: 2 })
+
+        // P2 chooses J from first row
+        game = options.find(option(0, 1))!.commit()
+        expect(Game.scores(game)).to.deep.equal([13, 10])
+
+        options = Game.options(game)
+        expect(options).to.have.length(2)
+        expect(options.map(rowAndAmount)).to.deep.include({ row: 1, amount: 1 })
+        expect(options.map(rowAndAmount)).to.deep.include({ row: 1, amount: 2 })
+
+        // P1 chooses 2 and 8 from second row
+        game = options.find(option(1, 2))!.commit()
+        expect(Game.scores(game)).to.deep.equal([23, 10])
+
+        options = Game.options(game)
+        expect(options).to.have.length(1)
+        expect(options.map(rowAndAmount)).to.deep.include({ row: 1, amount: 1 })
+
+        // P2 chooses Q from second row
+        game = options.find(option(1, 1))!.commit()
+        expect(Game.scores(game)).to.deep.equal([23, 0])
+    })
+})
---
+++ src/nimcard/game.ts
@@ -0,0 +1,53 @@
+import { Board } from './board'
+
+export type Game = {
+    board: Board,
+    currentPlayer: number,
+    players: Board.ScoredCard[][],
+}
+
+export namespace Game {
+    export type Option = {
+        row: number,
+        amount: number,
+        commit: () => Game,
+    }
+
+    const moveOption = (game: Game, row: number, amount: number): Option => {
+        return {
+            row, amount,
+            commit: () => {
+                const r = game.board[row]
+                const takenCards = r.slice(r.length - amount, r.length)
+
+                return {
+                    board: game.board.map((r, i) => r.slice(0, r.length - (i === row ? amount : 0))),
+                    currentPlayer: (game.currentPlayer + 1) % game.players.length,
+                    players: game.players.map((p, i) => p.concat(i === game.currentPlayer ? takenCards : [])),
+                }
+            }
+        }
+    }
+
+    export const start = (board: Board): Game => {
+        return {
+            board,
+            currentPlayer: 0,
+            players: [[], []],
+        }
+    }
+
+    export const options = (game: Game): Option[] => {
+        return game.board.flatMap((row, i) => {
+            const o: Option[] = []
+            if (row.length > 1) o.push(moveOption(game, i, 2))
+            if (row.length > 0) o.push(moveOption(game, i, 1))
+
+            return o
+        })
+    }
+
+    const sumBy = <T>(val: (it: T) => number) => (sum: number, cur: T) => sum + val(cur)
+    export const scores = (game: Game): number[] =>
+        game.players.map(player => player.reduce(sumBy(it => it.score), 0))
+}
