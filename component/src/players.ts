77686506541f093aa80c12d315920a6e0851e109
Timothy Foster <tf.auroratide@gmail.com>
2021-12-11 16:38:54 -0600

    Fallback if web workers are unavailable or the worker file is not provided

--- component/src/players.ts
+++ component/src/players.ts
@@ -8,22 +8,26 @@ export const ai = 'ai'
 export const doAiTurn = (game: Nimcard.Game, aiworker: string): Promise<Nimcard.Game.Option | null> => {
     const worker = new Worker(aiworker)
     const promise = new Promise<Nimcard.Game.Option | null>(resolve => {
-        worker.onmessage = (e: MessageEvent<{ row: number, amount: number } | null>) => {
-            if (e.data) {
-                resolve({
-                    row: e.data.row,
-                    amount: e.data.amount,
-                    commit: Nimcard.Game.makeCommit(game, e.data.row, e.data.amount)
-                })
-            } else {
-                resolve(null)
+        if (window.Worker && aiworker !== '') {
+            worker.onmessage = (e: MessageEvent<{ row: number, amount: number } | null>) => {
+                if (e.data) {
+                    resolve({
+                        row: e.data.row,
+                        amount: e.data.amount,
+                        commit: Nimcard.Game.makeCommit(game, e.data.row, e.data.amount)
+                    })
+                } else {
+                    resolve(null)
+                }
             }
-        }
 
-        worker.postMessage({
-            game: game,
-            depth: 20,
-        })
+            worker.postMessage({
+                game: game,
+                depth: 20,
+            })
+        } else {
+            resolve(Nimcard.Ai.optimal(20)(game))
+        }
     })
 
     return promise
